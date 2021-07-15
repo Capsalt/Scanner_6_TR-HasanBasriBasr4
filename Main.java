b71e0bb63c97f86b23817048e786d4e6a1b27d08
Hasan Basri Aygümüş <hbasri2304@gmail.com>
2021-07-15 21:25:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+  
+  Scanner scan = new Scanner(System.in);
+	
+	float x =scan.nextFloat();
+	
+	short y = (short)x;
+	
+	System.out.println(y);
+
+
+
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Kullanıcıdan bir Float değer girmesini isteyin, bu sayıyı short değişken tipine dönüştürün ve konsolda yazdırın.
